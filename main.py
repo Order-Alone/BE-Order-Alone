80a4e38a65a9072d475d31da40a806352163f8a6
dohwi <dohwe0978@gmail.com>
2026-01-13 15:07:55 +0900

    feat : cors 허용

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import api_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://13.209.210.38:5173",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api")
